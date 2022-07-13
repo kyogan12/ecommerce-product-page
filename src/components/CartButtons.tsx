1bf0fbc5a8a19d0aff99332e481a157d1f0e2e70
kyogan12 <kyoyoyo122@gmail.com>
2022-07-13 02:20:53 +0200

    Optimization for phones left

--- src/components/CartButtons.tsx
+++ src/components/CartButtons.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
-import CartLogo from "../assets/icon-cart.svg";
-import Minus from "../assets/icon-minus.svg";
-import Plus from "../assets/icon-plus.svg";
+import cartLogo from "../assets/icon-cart.svg";
+import minus from "../assets/icon-minus.svg";
+import plus from "../assets/icon-plus.svg";
 import { useShoppingCart } from "../context/ShoppingCartContext";
 
 export const CartButtons: React.FC = () => {
   const [counter, setCounter] = useState<number>(0);
-  const { increaseCartQuantity, removeFromCart } = useShoppingCart();
+  const { increaseCartQuantity } = useShoppingCart();
 
   const handleDecrement: Function = (): void => {
     counter === 0 ? setCounter(0) : setCounter(counter - 1);
@@ -15,12 +15,12 @@ export const CartButtons: React.FC = () => {
   return (
     <div className="btn-container">
       <div className="amount">
-        <img onClick={() => handleDecrement()} src={Minus} alt="minus" />
+        <img onClick={() => handleDecrement()} src={minus} alt="minus" />
         <p className="counter">{counter}</p>
-        <img onClick={() => setCounter(counter + 1)} src={Plus} alt="plus" />
+        <img onClick={() => setCounter(counter + 1)} src={plus} alt="plus" />
       </div>
       <div className="add-btn" onClick={() => increaseCartQuantity(counter)}>
-        <img className="logo" src={CartLogo} alt="cart" />
+        <img className="logo" src={cartLogo} alt="cart" />
         <p>Add to cart</p>
       </div>
     </div>
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,14 @@
 import Logo from "../assets/logo.svg";
 import Cart from "../assets/icon-cart.svg";
 import Avatar from "../assets/image-avatar.png";
+import cartThumbnail from "../assets/image-product-1-thumbnail.jpg";
+import bin from "../assets/icon-delete.svg";
 import { useShoppingCart } from "../context/ShoppingCartContext";
+import { useState } from "react";
 
 export const Nav: React.FC = () => {
-  const { getItemQuantity } = useShoppingCart();
+  const [showCart, setShowCart] = useState<boolean>(false);
+  const { getItemQuantity, removeFromCart } = useShoppingCart();
   const quantity = getItemQuantity();
 
   return (
@@ -20,9 +24,44 @@ export const Nav: React.FC = () => {
         <p className="link">Contact</p>
       </div>
       <div className="nav-right">
-        <div>
-          {quantity}
-          <img className="cart" src={Cart} alt="cart" />
+        <div className="cart">
+          {showCart && (
+            <div className="cart-items">
+              <p className="cart-items-p">Cart</p>
+              {quantity === 0 ? (
+                <p className="empty-cart">Your cart is empty.</p>
+              ) : (
+                <>
+                  <div className="inside-cart">
+                    <img src={cartThumbnail} alt="thumbnail" />
+                    <div>
+                      <p className="cart-name">Fall Limited Edition Sneakers</p>
+                      <p>
+                        $125.00 x {quantity}
+                        <b className="bold-price"> ${125 * quantity}</b>
+                      </p>
+                    </div>
+                    <img
+                      alt="bin"
+                      onClick={() => removeFromCart()}
+                      src={bin}
+                      className="bin"
+                    />
+                  </div>
+                  <div className="checkout-cart">
+                    <p>Checkout</p>
+                  </div>
+                </>
+              )}
+            </div>
+          )}
+          <img
+            onClick={() => setShowCart(!showCart)}
+            className="cart-icon"
+            src={Cart}
+            alt="cart"
+          />
+          <p className="quantity">{quantity}</p>
         </div>
         <img className="avatar" src={Avatar} alt="avatar" />
       </div>
